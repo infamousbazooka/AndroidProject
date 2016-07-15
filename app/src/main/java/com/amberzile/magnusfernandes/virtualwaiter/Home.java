9a78c229592ceffed2414fa89c53d2899f47fdde
infamousbazooka <magnusfernandes1295@gmail.com>
2016-07-15 21:29:02 +0530

    Initial Stage

---
+++ app/src/main/java/com/amberzile/magnusfernandes/virtualwaiter/Home.java
@@ -0,0 +1,28 @@
+package com.amberzile.magnusfernandes.virtualwaiter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(Home.this, "Yo", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+}
